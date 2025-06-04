51228cc935f359ff31a66b7aa3a76f2743da0712
RubenBarbeiro <202100668@estudantes.ips.pt>
2025-06-04 19:26:43 +0100

    Adicionado de icons para página minha banca, como começo de desenvolvimento e estruturação do tema geral da aplicação.

---
+++ agroconnect/lib/pages/minha_banca.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MinhaBanca extends StatelessWidget {
+  const MinhaBanca({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar_minha_banca(),
+      //body: ,
+
+    );
+  }
+
+  AppBar appBar_minha_banca() {
+    return AppBar(
+      title: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SvgPicture.asset(
+            'assets/icons/shop_header_icon.svg',
+            height: 20,
+            width: 20,
+          ),
+          SizedBox(width: 8), //adiciona espaço entre icon e texto
+          Text('Minha Banca')
+        ],
+      ),
+      leading: GestureDetector(
+        onTap: () {
+        //## completar com código para voltar pag anterior
+          //Navigator.pop(context);
+        },
+        child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset(
+            'assets/icons/back_arrow_appbar.svg',
+            height: 20,
+            width: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
