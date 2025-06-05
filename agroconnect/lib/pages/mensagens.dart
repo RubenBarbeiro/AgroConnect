8ec3272dc805cc629390cb2812d86f41243d0f22
ATeixeira <202100653@estudantes.ips.pt>
2025-06-06 00:18:58 +0100

    criação do file/página mensagens.dart

---
+++ agroconnect/lib/pages/mensagens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Mensagens extends StatelessWidget {
+  const Mensagens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: appBar_minha_banca(),
+      //body: ,
+
+    );
+  }
+}
